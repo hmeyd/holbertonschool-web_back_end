9ee27e8a9171f1707576b62598b8f5e12b64a788
hmeyd <yyahmedsalm@example.com>
2025-03-28 00:41:29 +0100

    le retse de task

---
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,12 @@
+function getCurrentYear() {
+    return new Date().getFullYear();
+  }
+  
+  export default function getBudgetForCurrentYear(income, gdp, capita) {
+    return {
+      [`income-${getCurrentYear()}`]: income,
+      [`gdp-${getCurrentYear()}`]: gdp,
+      [`capita-${getCurrentYear()}`]: capita,
+    };
+  }
+  
