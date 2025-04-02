7b34ebb315a8f75bf96ce262c6330bbf3c360a3b
hmeyd <yyahmedsalm@example.com>
2025-04-02 04:18:10 +0200

    task 9

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,13 +1,12 @@
 /* eslint-disable */
 export default function guardrail(mathFunction) {
     const queue = [];
-
     try {
-        queue.push(mathFunction);
+      queue.push(mathFunction());
     } catch (err) {
-        queue.push(err);
+      queue.push(`${err}`);
     }
-
     queue.push('Guardrail was processed');
     return queue;
-}
+  }
+  
