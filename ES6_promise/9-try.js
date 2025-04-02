adf0a0dbee5bfab70b4b16fa99949dd8d0d9c053
hmeyd <yyahmedsalm@example.com>
2025-04-02 04:07:52 +0200

    task 9

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -3,10 +3,9 @@ export default function guardrail(mathFunction) {
     const queue = [];
 
     try {
-        const result = mathFunction();
-        queue.push(result);
+        queue.push(mathFunction);
     } catch (error) {
-        queue.push(error.message);
+        queue.push(error);
     }
 
     queue.push("Guardrail was processed");
