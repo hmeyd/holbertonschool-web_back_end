a5b3c7d298dc94046978a9575db9cb307fab6b20
hmeyd <yyahmedsalm@example.com>
2025-04-02 04:05:01 +0200

    task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,15 +1,13 @@
 /* eslint-disable */
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([
-        signUpUser(firstName, lastName),
-        uploadPhoto(fileName)
-    ]).then((results) =>
-        results.map((result) => ({
-            status: result.status,
-            value: result.status === 'fulfilled' ? result.value : result.reason
-        }))
-    );
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((promise) => ({
+      status: promise.status,
+      value: promise.status === 'fulfilled'
+        ? promise.value
+        : `${promise.reason.name}: ${promise.reason.message}`,
+    })));
 }
