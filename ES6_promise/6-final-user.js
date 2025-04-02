97c5621b7b6edb04eb281af232d32f5cd734c98c
hmeyd <yyahmedsalm@example.com>
2025-04-02 03:55:08 +0200

    task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,14 +2,17 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+    const results = await Promise.allSettled([
         signUpUser(firstName, lastName),
         uploadPhoto(fileName)
-    ]).then((results) => 
-        results.map((result) => ({
-            status: result.status,
-            value: result.status === 'fulfilled' ? result.value : result.reason
-        }))
-    );
+    ]);
+
+    return results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason
+    }));
 }
