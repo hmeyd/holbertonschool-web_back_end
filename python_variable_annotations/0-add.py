cc27bdc66acd53c10f28955dc3e281098f3b849d
hmeyd <yyahmedsalm@example.com>
2025-04-24 12:24:30 +0200

    projet

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+"""Module for add function"""
+
+
 def add(a: float, b: float) -> float:
+    """
+    Add two numbers together.
+    :param a: The first number.
+    :param b: The second number.
+    :return: The sum of the two numbers.
+    """
     return a + b
