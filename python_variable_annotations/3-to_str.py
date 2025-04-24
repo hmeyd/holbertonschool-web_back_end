ebccbbd98f24a02196f11b8d8cf14c3b8d7663df
hmeyd <yyahmedsalm@example.com>
2025-04-24 13:44:50 +0200

    commentaire

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -5,11 +5,5 @@
 def to_str(x: float) -> str:
     """
     Convert a float to a string.
-    
-    Args:
-        x (float): The number to be converted.
-    
-    Returns:
-        str: The string representation of the number.
     """
     return str(x)
--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 """Module for variable annotations"""
-
-
 from typing import List
+
+
 def sum_mixed_list(mxd_lst: List[float]) -> float:
     """
     Returns the sum of a mixed list of integers and floats.
-
-    Args:
-        mxd_lst (list[float]): A list containing integers and floats to be summed.
-
-    Returns:
-        float: The sum of the list.
     """
     return sum(mxd_lst)
