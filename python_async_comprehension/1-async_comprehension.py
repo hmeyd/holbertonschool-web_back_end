e9b412fcaa6aedb5165db4d19669f1700b6f4535
hmeyd <yyahmedsalm@example.com>
2025-05-01 13:58:38 +0200

    task1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Module for async_comprehension coroutine."""
 
-import asyncio
 from typing import List
-from 0-async_generator import async_generator # Assuming this is in the same directory
+async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
-    """Collect 10 random floats from async_generator and return them in a list."""
+    """Collect 10 random floats from async_generator
+    and return them in a list."""
     return [i async for i in async_generator()]
