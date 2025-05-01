f8494ee5dcf59a0ca268fca2deed3a4e52a16564
hmeyd <yyahmedsalm@example.com>
2025-05-01 14:13:53 +0200

    tout

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -11,11 +11,6 @@ async def measure_runtime() -> float:
     """Measure the total time taken to run async_comprehension
     four times in parallel."""
     start_time = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
     end_time = time.time()
     return end_time - start_time
