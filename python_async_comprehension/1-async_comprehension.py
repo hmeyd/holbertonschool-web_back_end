2ec9f6c56613b7a2cfbe6f4a79f469c6487df899
hmeyd <yyahmedsalm@example.com>
2025-05-01 14:02:58 +0200

    recu

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,6 +5,7 @@
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List[float]:
     """Collect 10 random floats from async_generator
     and return them in a list."""
