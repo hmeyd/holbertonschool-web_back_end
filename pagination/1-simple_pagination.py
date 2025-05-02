620df7bc296a25f6c68577c6886e9b2e4a8278c8
hmeyd <yyahmedsalm@example.com>
2025-05-02 03:31:58 +0200

    task 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python3
+"""Module for index_range function and Server class"""
 import csv
+import math
 from typing import List, Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Return a tuple of start and end index for a given
-    page and page_size."""
-    start = (page - 1) * page_size
-    end = page * page_size
-    return (start, end)
+def index_range(page: int, page_size: int) -> Tuple[int]:
+    """Return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list
+    for those pagination parameters"""
+    end_index = page * page_size
+    start_index = end_index - page_size
+    return (start_index, end_index)
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -29,14 +34,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Return the appropriate page of the dataset."""
+        """Return a list of data for the page"""
         assert type(page) is int and type(page_size) is int
         assert page > 0 and page_size > 0
 
         start_index, end_index = index_range(page, page_size)
-        dataset = self.dataset()
 
-        if start_index >= len(dataset):
-            return []
-
-        return dataset[start_index:end_index]
+        return [] if start_index >= len(self.dataset()) \
+            else self.dataset()[start_index:end_index]
+    
