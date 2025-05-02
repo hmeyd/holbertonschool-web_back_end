6ae149b33e0b53b3b244b2841c3cd35ef5b90831
hmeyd <yyahmedsalm@example.com>
2025-05-02 03:37:35 +0200

    task 2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,42 +1,47 @@
 #!/usr/bin/env python3
+"""Module for index_range function and Server class"""
 import csv
 import math
-from typing import List, Tuple, Dict, Any
+from typing import List, Tuple, Dict
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Return a tuple of start and end index for a given page and page_size."""
-    start = (page - 1) * page_size
-    end = page * page_size
-    return (start, end)
+def index_range(page: int, page_size: int) -> Tuple[int]:
+    """Return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list
+    for those pagination parameters"""
+    end_index = page * page_size
+    start_index = end_index - page_size
+    return (start_index, end_index)
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset = dataset[1:]  # skip header
+            self.__dataset = dataset[1:]
+
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Return the appropriate page of the dataset."""
-        assert isinstance(page, int), "page must be an integer"
-        assert page > 0, "page must be > 0"
-        assert isinstance(page_size, int), "page_size must be an integer"
-        assert page_size > 0, "page_size must be > 0"
+        """Return a list of data for the page"""
+        assert type(page) is int and type(page_size) is int
+        assert page > 0 and page_size > 0
 
         start_index, end_index = index_range(page, page_size)
-        dataset = self.dataset()
-        return dataset[start_index:end_index]
+
+        return [] if start_index >= len(self.dataset()) \
+            else self.dataset()[start_index:end_index]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """Return a dict of data for the page"""
