7c2170bc09a5a17fe2ce3367a1ef2795eeb5c98c
hmeyd <yyahmedsalm@example.com>
2025-05-02 14:50:40 +0200

    task 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
-'''Provides some stats stored in MongoDB'''
-
-
+"""Module for log_stats function"""
 from pymongo import MongoClient
 
 
 def log_stats():
-    '''Provides some stats'''
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    """provides some stats about Nginx logs stored in MongoDB"""
+    client = MongoClient('localhost', 27017)
     db = client.logs
     collection = db.nginx
 
-    num_logs = collection.count_documents({})
-    print(f"{num_logs} logs")
-
-    print("Methods:")
+    logs_qty = collection.count_documents({})
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        value = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {value}")
+    status_check = collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+        })
 
-    status_log = collection.count_documents(
-        {"method": "GET", "path": "/status"})
-    print(f"{status_log} status check")
+    print(f"{logs_qty} logs")
+    print("Methods:")
+    for method in methods:
+        method_qty = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_qty}")
+    print(f"{status_check} status check")
 
 
 if __name__ == "__main__":
