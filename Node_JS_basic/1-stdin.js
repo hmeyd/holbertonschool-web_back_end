1f72e01f3cc2ea29756ca035ac181abfea4236eb
hmeyd <yyahmedsalm@example.com>
2025-05-23 01:07:15 +0200

    correction dans le syntax de task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,13 @@
 process.stdout.write("Welcome to Holberton School, what is your name?\n")
 process.stdin.setEncoding('utf8')
 process.stdin.on('data', function (data) {
-    const name = data.trim();
+    const name =data.trim();
+    if (name === 'exit') {
+        process.stdin.end();
+        return;
+    }
     console.log(`Your name is: ${name}`);
-    console.log('This important software is now closing')
-    process.stdin.end();
+});
+process.stdin.on('end', function (){
+    console.log('This important software is now closing');
 });
