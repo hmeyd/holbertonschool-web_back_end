b9ef60a6119f393c75d85673157152222a9ab233
hmeyd <yyahmedsalm@example.com>
2025-05-23 01:11:37 +0200

    changer le syntax de la function =>

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,10 @@
 process.stdout.write("Welcome to Holberton School, what is your name?\n")
 
-process.stdin.on('data', function (data) {
-    const name =data.trim();
-    if (name === 'exit') {
-        process.stdin.end();
-        return;
-    }
-    console.log(`Your name is: ${name}`);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 process.stdin.on('end', function (){
     console.log('This important software is now closing');
